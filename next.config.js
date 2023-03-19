2828e3d8b485e6105074d712ce67c4d8bff43afc
Andreas Haessler <andreas.haessler@outlook.at>
2023-03-19 22:49:30 +0100

    Add Rust module

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    return { ...config, experiments: { asyncWebAssembly: true } };
+  },
 };
 
 module.exports = nextConfig;
