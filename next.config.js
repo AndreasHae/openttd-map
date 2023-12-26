2efe8ad9b12c1d65686a7b393144a6808d8c3385
Andreas Haessler <andreas.haessler@outlook.at>
2023-12-27 00:04:00 +0100

    Add Github Action

--- next.config.js
+++ next.config.js
@@ -1,11 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   transpilePackages: ["@react-sigma/core"],
-  webpack: (config) => {
-    config.experiments.asyncWebAssembly = true;
+  webpack(config, { isServer, dev }) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+
+    // Next.js Webassembly workaround by GitHub user sam3d
+    // https://github.com/vercel/next.js/issues/29362#issuecomment-971377869
+    if (!dev && isServer) {
+      config.output.webassemblyModuleFilename = "chunks/[id].wasm";
+      config.plugins.push(new WasmChunksFixPlugin());
+    }
+
     return config;
   },
 };
+
+class WasmChunksFixPlugin {
+  apply(compiler) {
+    compiler.hooks.thisCompilation.tap("WasmChunksFixPlugin", (compilation) => {
+      compilation.hooks.processAssets.tap({ name: "WasmChunksFixPlugin" }, (assets) =>
+        Object.entries(assets).forEach(([pathname, source]) => {
+          if (!pathname.match(/\.wasm$/)) return;
+          compilation.deleteAsset(pathname);
+
+          const name = pathname.split("/")[1];
+          const info = compilation.assetsInfo.get(pathname);
+          compilation.emitAsset(name, source, info);
+        }),
+      );
+    });
+  }
+}
 
 module.exports = nextConfig;
