ed34d967b9bc0b67f2928f82be1f546c6ecc5474
Andreas Hässler <andreas.haessler@outlook.at>
2024-12-02 23:45:40 +0100

    Disable prerendering for home page

--- app/components/home-page.tsx
+++ app/components/home-page.tsx
@@ -8,66 +8,69 @@ import { Map } from "./map";
 import dynamic from "next/dynamic";
 import { useDropzone } from "react-dropzone";
 
-export const HomePage = dynamic(async () => {
-  const wasm = await import("../../savegame-reader/pkg");
+export const HomePage = dynamic(
+  async () => {
+    const wasm = await import("../../savegame-reader/pkg");
 
-  return () => {
-    const [graph, setGraph] = useState<Graph | undefined>();
+    return () => {
+      const [graph, setGraph] = useState<Graph | undefined>();
 
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({
-      onDropAccepted: async (droppedFiles) => {
-        await loadGraph(droppedFiles[0]);
-      },
-    });
+      const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDropAccepted: async (droppedFiles) => {
+          await loadGraph(droppedFiles[0]);
+        },
+      });
 
-    async function loadGraph(file: File): Promise<void> {
-      const graph = new Graph({ type: "directed", multi: false, allowSelfLoops: false });
+      async function loadGraph(file: File): Promise<void> {
+        const graph = new Graph({ type: "directed", multi: false, allowSelfLoops: false });
 
-      const buf = new Uint8Array(await file.arrayBuffer());
-      const allGraphs: LinkGraph[] = JSON.parse(wasm.load_file(buf));
-      const passengerGraphs = allGraphs.filter((graph) => graph.cargo === 0);
+        const buf = new Uint8Array(await file.arrayBuffer());
+        const allGraphs: LinkGraph[] = JSON.parse(wasm.load_file(buf));
+        const passengerGraphs = allGraphs.filter((graph) => graph.cargo === 0);
 
-      for (const nodes of passengerGraphs.map((graph) => graph.nodes)) {
-        for (const node of nodes) {
-          // TODO read from savefile
-          const mapSizeX = 1024;
-          const mapSizeY = 1024;
+        for (const nodes of passengerGraphs.map((graph) => graph.nodes)) {
+          for (const node of nodes) {
+            // TODO read from savefile
+            const mapSizeX = 1024;
+            const mapSizeY = 1024;
 
-          const coords = deserializeCoordinates(node, mapSizeX);
-          graph.mergeNode(node.station, transposeCoordinates(mapSizeX, coords, mapSizeY));
+            const coords = deserializeCoordinates(node, mapSizeX);
+            graph.mergeNode(node.station, transposeCoordinates(mapSizeX, coords, mapSizeY));
 
-          for (const edge of node.edges) {
-            if ((edge.next_edge ?? edge.dest_node) === 65535) break;
+            for (const edge of node.edges) {
+              if ((edge.next_edge ?? edge.dest_node) === 65535) break;
 
-            const destination = nodes[(edge.next_edge ?? edge.dest_node)];
-            graph.mergeNode(destination.station);
-            graph.addEdge(node.station, destination.station);
+              const destination = nodes[edge.next_edge ?? edge.dest_node];
+              graph.mergeNode(destination.station);
+              graph.addEdge(node.station, destination.station);
+            }
           }
         }
-      }
 
-      setGraph(graph);
-    }
-    return (
-      <main className={"w-screen h-screen" + (graph ? "" : " p-2")}>
-        {graph ? (
-          <Map graph={graph} />
-        ) : (
-          <div
-            {...getRootProps()}
-            className={
-              "w-full h-full border-2 border-dashed flex flex-col items-center justify-center gap-4 cursor-pointer" +
-              (isDragActive ? " border-blue-500" : "")
-            }
-          >
-            <h1 className="text-5xl font-bold">OpenTTD Map Visualizer</h1>
-            <p className="text-xl">
-              Drag and drop your <code>.sav</code> file here or click anywhere to select file
-            </p>
-            <input {...getInputProps()} />
-          </div>
-        )}
-      </main>
-    );
-  };
-});
+        setGraph(graph);
+      }
+      return (
+        <main className={"w-screen h-screen" + (graph ? "" : " p-2")}>
+          {graph ? (
+            <Map graph={graph} />
+          ) : (
+            <div
+              {...getRootProps()}
+              className={
+                "w-full h-full border-2 border-dashed flex flex-col items-center justify-center gap-4 cursor-pointer" +
+                (isDragActive ? " border-blue-500" : "")
+              }
+            >
+              <h1 className="text-5xl font-bold">OpenTTD Map Visualizer</h1>
+              <p className="text-xl">
+                Drag and drop your <code>.sav</code> file here or click anywhere to select file
+              </p>
+              <input {...getInputProps()} />
+            </div>
+          )}
+        </main>
+      );
+    };
+  },
+  { ssr: false },
+);
